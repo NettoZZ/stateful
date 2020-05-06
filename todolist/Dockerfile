027682e5213d36dabeaaa5b2a6f0b5951c6ca566
NettoZZ <62664647+NettoZZ@users.noreply.github.com>
2020-05-06 15:02:34 -0300

    Add files via upload

---
+++ todolist/Dockerfile
@@ -0,0 +1,6 @@
+FROM php:apache
+RUN sed 's/80/${PORT}/g' -i /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
+ENV PORT=80                                                                                                               
+COPY index.php . 
+COPY new.php . 
+COPY task.txt .
--- todolist/new.php
+++ todolist/new.php
@@ -14,33 +14,5 @@ $texto = fread($fp, filesize($arquivo));
 $texto = nl2br($texto);
 ?>
 <?php
-header('Location: /');
+header('Location: https://n4528.herokuapp.com/');
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title> Lista PHP </title>
-</head>
-<body>
-    <div class="heading">   
-        <h2> Minha Lista </h2>
-    </div> 
-    <form method="POST" action="new.php">
-        <input type="text" name="task" class="task_input">
-        <button href="new" type="submit" class="add_bnt" name="submit">Adicionar</button>
-    </form> <br>
-    <table>
-        <thead>
-            <tr>
-                <th>Tarefas</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class= "task"><p class="teste"> <? echo mb_strtolower( $texto, 'UTF-8' ); ?> </p></th>
-            </tr>
-        </tbody>
-    </table>
-</body>
-</html>
